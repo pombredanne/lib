40f4781df8a0ea1e29c565dedfd363b91e0189d3
sakitake <k.take4.0709@gmail.com>
2012-06-19 14:44:49 +0900

    github対応。

--- js/codefetch/codefetch.js
+++ js/codefetch/codefetch.js
@@ -1,14 +1,17 @@
 (function(jQury) {
-    $.fn.codefetch = function(url, lang) {
+    $.fn.codefetch = function(user, repos, path, lang) {
         var _this = this;
+        var url = "https://api.github.com/repos/" + user + "/" + repos + "/contents/" + path
 
         $.ajax({
             url: url,
-            dataType: 'text',
             async: false,
-            success: function(text) {
-                text = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-                _this.append(text);
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Accept", "application/vnd.github.beta.raw");
+            },
+            success: function(content) {
+                content = content.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+                _this.append(content);
                 _this.attr("class", "brush:" + lang);
             },
             error: function() {
@@ -16,6 +19,7 @@
                 _this.attr("class", "brush:" + lang);
             }
         });    
+
     }
 
 })(jQuery);
